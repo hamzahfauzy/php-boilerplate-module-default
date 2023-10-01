90677cb7fdaa72f38c5e91c87d831db53b5dd473
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-10-01 12:01:56 +0700

    add guard

---
+++ guards/index.php
@@ -0,0 +1,11 @@
+<?php
+$auth = auth();
+$authRoute = [
+    'auth/login',
+    'auth/logout',
+];
+
+if(!in_array($route, $authRoute) && !is_allowed($route, $auth->id))
+{
+    die('Error 403. Unauthorized');
+}
