7815ddba29398f7aa5224f934fa91551e806ca6a
root <root@idc30.m2server.my.id>
2024-09-18 03:56:30 -0400

    update font size icon

--- config/menu.php
+++ config/menu.php
@@ -3,31 +3,31 @@
 return [
     [
         'label' => 'default.menu.media',
-        'icon'  => 'fa-fw fa-xl me-2 fa-solid fa-photo-film',
+        'icon'  => 'fa-fw fa-lg me-2 fa-solid fa-photo-film',
         'route' => routeTo('crud/index',['table' => 'media']),
         'activeState' => 'default.media'
     ],
     [
         'label' => 'default.menu.calendar',
-        'icon'  => 'fa-fw fa-xl me-2 fa-solid fa-calendar',
+        'icon'  => 'fa-fw fa-lg me-2 fa-solid fa-calendar',
         'route' => routeTo('crud/index',['table' => 'calendar']),
         'activeState' => 'default.calendar'
     ],
     [
         'label' => 'default.menu.users',
-        'icon'  => 'fa-fw fa-xl me-2 fa-solid fa-users',
+        'icon'  => 'fa-fw fa-lg me-2 fa-solid fa-users',
         'route' => routeTo('crud/index',['table'=>'users']),
         'activeState' => 'default.users'
     ],
     [
         'label' => 'default.menu.roles',
-        'icon'  => 'fa-fw fa-xl me-2 fa-solid fa-cubes',
+        'icon'  => 'fa-fw fa-lg me-2 fa-solid fa-cubes',
         'route' => routeTo('crud/index',['table'=>'roles']),
         'activeState' => 'default.roles'
     ],
     [
         'label' => 'default.menu.settings',
-        'icon'  => 'fa-fw fa-xl me-2 fa-solid fa-cog',
+        'icon'  => 'fa-fw fa-lg me-2 fa-solid fa-cog',
         'route' => routeTo('default/settings/index'),
         'activeState' => 'default.settings'
     ],
